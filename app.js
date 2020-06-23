9410ea8747aa2cd3d528da8b7f19d644b65288a8
WahajUlHaq <64356149+WahajUlHaq@users.noreply.github.com>
2020-06-23 21:50:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,261 @@
+
+// // ALERTS CHAP  1
+
+// alert("CHAPTER 1 Task Start From Here");// section 1 
+
+
+// alert("Please! Enter A Valid Password TASK 1");// task 2
+
+// alert("Welcome To JS LAND !" + '\n' + "HAPPY CODING!" + '\n' + "TASK 2");// task 3
+
+
+// alert("Welcome To Js Land");// task 4
+
+// alert("Happy Coding !")
+
+
+// alert("Hello... I can Js through my browser console task no 5") //i do it in web console and printed a alert and it is task 5 
+
+
+
+
+// var end1 = "Chap 1 END";
+// alert(end1)
+
+// // VARIABLE CHAP 2
+
+
+// var username = "Wahaj Ul Haq";
+// var myname = "WAHAJ UL HAQ STUDENT OF SAYLANI"; // task 1 chap 2
+// alert(myname);
+
+
+
+// var message = "HELLO WORLD";// task 2 chap 2
+
+// alert(message);
+
+
+
+
+
+// var studentBio1 = "Jhone Deo";// task 3 chap 2
+// var studentBio3 = "15 Years Old";
+// var studentBio4 = "Certified Developer";
+// alert(studentBio1);
+// alert(studentBio3);
+// alert(studentBio4);
+
+
+
+
+// alert("pizza" + '\n' + "pizz" + '\n' + "piz" + '\n' + "pi" + '\n' + "p");// task 4 chap 2
+
+
+
+
+
+// var email = "example@example.com";// task 5 chap 2
+// var text = "My email address";
+// alert(text + " " + email);
+
+
+
+// var A = "A smarter way to learn JavaScript";// task 6 chap 2
+// alert(A);
+
+
+
+
+
+// var bro = "Thanks Saylani I Can Write Also In Browser";// task 7 chap 2
+// document.write(bro);
+
+
+// // task 8 chap 2
+// var sym = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(sym)
+
+
+
+// var end2 = "Chap 2 END";
+// alert(end2)
+
+
+// Chap 2 END
+
+
+
+// Chap 3 START
+
+// task 1
+
+
+// var age="17";
+// alert("i am" +" "+age+" "+"years old");
+
+
+
+// task 2
+
+
+
+
+
+
+
+// task 3
+
+
+// var birthYear="17";
+// document.write("My Birth Year Is" + " " + birthYear);
+
+
+
+// task 4
+
+// var name = prompt("Please enter your  Name");
+// var on = prompt("Please enter your Order name");
+// var qty = prompt("Please enter your Quantity");
+//   document.write(name +" "+ "order"+" " + on +" "+"in Quantity" +" " + qty + " " + "From Wahaj Store");
+
+
+// chapter 3 end
+
+
+
+// chapter 4 start
+
+// task 1
+
+
+
+
+
+
+
+// chapter 5 start 
+
+// task 1
+
+
+// var a=10;
+// var b=10;
+// var c =a+b;
+// var d=a/b;
+// var e=a-b;
+// var f=a*b;
+// document.write("Sum Of" + " " + a + " " + "+" + " " + b + " "+ "Is" + " " + c + "<br>");
+// document.write("Multiplication Of" + " " + a + " " + "x" + " " + b + " "+ "Is" + " " + f + "<br>");
+// document.write("Division Of" + " " + a + " " + "/" + " " + b + " "+ "Is" + " " + d + "<br>");
+// document.write("Subtraction Of" + " " + a + " " + "-" + " " + b + " "+ "Is" + " " + e + "<br>");
+
+
+// task 2
+
+
+
+
+
+// task 3
+
+
+
+// var a=600;
+// var b = prompt("Please enter your  Ticket Quanitity");
+// document.write("Price For Ticket In Quanitity " + " " + b + " "   + "is "  + " " +  a*b);
+
+
+
+
+// task 4
+
+
+// var b=2;
+// for ( var a=1; a<= 100; a++){
+
+
+// document.write(b + " " + "x" + " " + a + " " + "=" + b*a +  "<br>")
+
+// }
+
+
+
+
+// task 5
+
+// var c = prompt("Please enter your  F To Be Converted To c");
+// var ftc=(5/9);
+// var ftc1=(32);
+// var ftc2=(c-ftc1);
+// var ftc3 = (ftc2*ftc);
+// document.write(ftc3+"  "+  "C"+"<br>");
+
+
+// var ca = prompt("Please enter your  c To Be Converted To f");
+// var ftca=(9/5);
+// var ftc1a=(32);
+// var ftc2a=(ca*ftca);
+// var ftc3a = (ftc2a+ftc1a);
+// document.write(ftc3a +"  "+"F");
+
+
+
+// task 6
+
+
+// var i1=prompt("ENTER PRODUCT 1 PRICE");
+// var iq1=prompt("ENTER PRODUCT 1  QTY ");
+// var is1=prompt("ENTER PRODUCT 1 SHIPPING PRICE");
+// var ia11=i1+is1;
+// var ia1=ia11*iq1;
+// var i2=prompt("ENTER PRODUCT 2 Price");
+// var iq2=prompt("ENTER PRODUCT 2 Qty");
+// var is2=prompt("ENTER PRODUCT 2 SHIPPING PRICE");
+// var ia12=i2+is2;
+// var ia2=ia12*iq2;
+// var ta=ia1+ia2;
+// document.write("CHARGES OF PRODUCT 1 " + " " + i1 + " " + "RS" + "<br>");
+// document.write("CHARGES OF PRODUCT 1 SHIPPING  " + " " + is1 + " " + "RS" + "<br>");
+// document.write("QUANTITY OF PRODUCT 1 " + " " + iq1 + " " + "RS" + "<br>" + "<br>");
+
+
+
+
+// document.write("CHARGES OF PRODUCT 2 " + " " + i2 + " " + "RS" + "<br>");
+// document.write("CHARGES OF PRODUCT 2 SHIPPING  " + " " + is2 + " " + "RS" + "<br>");
+// document.write("QUANTITY OF PRODUCT 1 " + " " + iq2 + " " + "RS" + "<br>" + "<br>");
+// document.write("TOTAL OF PRODUCT 1 " + " " + ia2 + " " + "RS" + "<br>" + "<br>" + "<br>");
+
+
+
+
+
+
+var item1 =40;
+var item1q =2;
+var item1s =10;
+
+
+var item2 =100;
+var item2q =3;
+var item2s =4;
+
+document.write("Price Of Item One Is" + " " + item1 +  " " +"<br>"+"<br>");
+document.write("Price Of Item Two QTY Is" + " "+  item1q+ " " + "<br>"+"<br>");
+
+
+
+document.write("Price Of Item Two Is" + " " + item2 +"" + "<br>"+"<br>");
+document.write("Price Of Item Two QTY Is" + " "+  item2q+ " " + "<br>"+"<br>");
+var itemt =item1s + item2s;
+var item1t = item1 + item1s * item1q;
+
+document.write("Item Shipping Total " + " "+ itemt+ "<br>"+"<br>");
+
+var item2t = item2 + item2s * item2q;
+document.write("Total Amount 1 " + " "+ item1t+ "<br>"+"<br>");
+document.write("Total Amount 2" + " "+ item2t+ "<br>"+"<br>");
+
+var itemat = item1t+item2t+itemt;
+document.write("Total Amount " + " "+ itemat+ "<br>"+"<br>");
